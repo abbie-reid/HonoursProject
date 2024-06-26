3a672f8d6b06c740fb2d0b7b55e3ebfc36f19e19
abbie-reid <abigailreidnz@gmail.com>
2024-06-26 16:40:16 +1200

    Fixed indentation and spelling

--- Bluetooth/BT_Server.py
+++ Bluetooth/BT_Server.py
@@ -10,12 +10,11 @@ server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
-jeston_nano_4_uuid = "00001800-0000-1000-8000-00805f9b34fb"
+jetson_nano_4_uuid = "00001800-0000-1000-8000-00805f9b34fb"
 server_name = "JetsonNano4"
 
-bluetooth.advertise_service(server_sock, server_name, service_id=uuid,
-	                    service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
-	                    profiles=[bluetooth.SERIAL_PORT_PROFILE],
+bluetooth.advertise_service(server_sock, server_name, service_id=jetson_nano_4_uuid,
+	                    service_classes=[jetson_nano_4_uuid, bluetooth.SERIAL_PORT_CLASS],
 	                    )
 
 print("Waiting for connection on RFCOMM channel", port)
@@ -24,13 +23,13 @@ client_sock, client_info = server_sock.accept()
 print("Accepted connection from", client_info)
 
 try:
-    while True:
-	data = client_sock.recv(1024)
-	if not data:
-	    break
-	print("Received", data)
+	while True:
+		data = client_sock.recv(1024)
+		if not data:
+		    break
+		print("Received", data)
 except OSError:
-    pass
+	pass
 
 print("Disconnected.")
 
